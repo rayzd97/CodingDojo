c34addc6367ef1bf029a1593fbbbedbf48fe5def
Bernardo Galindo <bernardo466@gmail.com>
2014-10-01 23:26:31 -0500

    allow \n custom delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,14 +1,19 @@
 class StringCalculator 
 	def initialize
 		@delimiter = nil
-		@regex = Regexp.new(/,|\n/)
+		@regex = regex(/,|\n/)
 		@negatives = "negative numbers not allowed"
 	end
 
 	def add(cadena)
 		return 0 if cadena.empty?
-		/^\/\/(?<delimiter>.)\n/.match(cadena) {|m| @delimiter = m[:delimiter] }
-		@regex = @delimiter.empty? ? "\n" : @delimiter if @delimiter
+		regex('^\/\/(?<delimiter>.|\s)\n').match(cadena) {|m| @delimiter = m[:delimiter] }
+		@regex = regex(@delimiter) if @delimiter
 		cadena.split(@regex).inject{|sum, n| return @negatives if n.to_i < 0; sum.to_i + n.to_i} 
 	end
+
+	private
+		def regex(str)
+			Regexp.new(str)
+		end
 end
