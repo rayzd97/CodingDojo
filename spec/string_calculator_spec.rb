875dfc843cf3644c753f647e13056e1a39de99b9
Bernardo Galindo <bernardo466@gmail.com>
2014-10-01 22:12:09 -0500

    accept \n as delimeter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe StringCalculator do
 		expect(string_calculator.add("1,2,3")).to eq(6)
 	end
 
+	it "receive an string with with numbers divide by \n sum and return total" do
+		expect(string_calculator.add("1\n2\n3")).to eq(6)
+	end
+
 end
